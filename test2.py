ff30074b3ea2de0c0726d1dfd5d97406190334f7
BK202 <45834794+xzj478@users.noreply.github.com>
2020-02-20 09:53:30 +0200

    Add files via upload

---
+++ test2.py
@@ -0,0 +1,10 @@
+x = int(input('Enter number:\n'))
+y = int(input('Enter another number:\n'))
+if x == y:
+    print('x and y are equal')
+else:
+    if x < y:
+        print('x is less than y')
+    else:
+        print('x is greater than y')
+    
